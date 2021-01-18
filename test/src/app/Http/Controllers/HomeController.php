a89a4f78445bddfa6315adb90e59dae792ee1929
HarAlb <59127805+HarAlb@users.noreply.github.com>
2021-01-18 22:31:04 +0400

    Update HomeController.php
    BUGFIX: Search request

--- test/src/app/Http/Controllers/HomeController.php
+++ test/src/app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Drug;
+
 class HomeController extends Controller
 {
     /**
@@ -27,33 +29,18 @@ class HomeController extends Controller
         $message = '';
         $count_substance = $req->substance ? count($req->substance) : 0;
         if($count_substance > 1){
-            $drug_substances = \App\Models\DrugSubstance::whereIn('substance_id' , $req->substance)
-            ->where('visible' , 1)
-            ->select('drug_id' , \DB::raw('group_concat(substance_id) as substances'))
-            ->groupBy('drug_id')
-            ->orderBy(\DB::raw('LENGTH(substances)') , 'DESC')
-            ->get();
-            $res = [];
-            
-            // Dont need $i but performace is more better then without index
-            foreach($drug_substances as $i => $drug_substance){
-                $count_intersect = count(array_intersect($req->substance , explode(',' , $drug_substance->substances)));
-                if($count_substance === $count_intersect){
-                    $res['target'][] = $drug_substance->drug_id;
-                };
-                if(!isset($res['target'])){
-                    if($count_intersect <= 2) continue;
-                    $res['have_pice'][] = ['drug' => $drug_substance->drug_id , 'count_intersect' => $count_intersect];
-                }
-            }
-            $res_ids = [];
-            if(isset($res['target'])){
-                $res_ids = $res['target'];
-            }elseif(isset($res['have_pice'])){
-                $res_ids = collect($res['have_pice'])->pluck('drug')->toArray();
+            $drugs = Drug::join('drug_substance' , 'drug_substance.drug_id' , 'drugs.id')
+                    ->leftJoin('substances' , 'substances.id' , 'drug_substance.substance_id')
+                    ->whereIn('substances.id' , $req->substance)
+                    ->select('drugs.name' , 'drugs.id' , \DB::raw('LENGTH(REGEXP_REPLACE(GROUP_CONCAT(substances.id)  , "[0-9]*" , "") ) + 1 as count_intersect'))
+                    ->groupBy('drug_substance.drug_id')
+                    ->with('substancesNotPrimary')
+                    ->get();
+            if($drugs->where('count_intersect' , $count_substance)->count()){
+                $drugs = $drugs->where('count_intersect' , $count_substance);
             }
             
-            $drugs = \App\Models\Drug::whereIn('id' , $res_ids)->orderBy(\DB::raw('FIELD(`id` , ' . implode(',' , $res_ids) . ')'))->with('substancesNotPrimary')->paginate()->appends(['substance' => $req->substance]);
+            $drugs =  \App\Helper\PaginateHelper::paginate($drugs->sortByDesc('count_intersect') , 5)->appends(['substance' => $req->substance]);
             if($drugs->count() <= 1){
                 $drugs = [];
                 $message = 'не найдено лекарств';
